00414ba0485ed77cf916162942026bf822c3b8e0
Rodrigo Santos Menezes <rodrigo.menezes@cnj.jus.br>
2017-08-16 09:37:44 -0300

    [PJE2-TREINA] Criando projeto

---
+++ src/main/java/br/jus/cnj/pje/cepservice/CepServiceApplication.java
@@ -0,0 +1,12 @@
+package br.jus.cnj.pje.cepservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepServiceApplication.class, args);
+	}
+}
