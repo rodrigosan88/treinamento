f6e98ccebcf7b499c69d4c5096a21cd24a615352
Rodrigo Santos Menezes <rodrigo.menezes@cnj.jus.br>
2017-08-16 22:55:35 -0300

    [PJE2-TREINA] Configurando projeto (dependencias)

--- src/main/java/br/jus/cnj/pje/cepservice/CepServiceApplication.java
+++ src/main/java/br/jus/cnj/pje/cepservice/CepServiceApplication.java
@@ -2,9 +2,13 @@ package br.jus.cnj.pje.cepservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class CepServiceApplication {
 
